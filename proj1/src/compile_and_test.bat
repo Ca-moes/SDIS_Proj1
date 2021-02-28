24a47b234c07feb299db5fbce0267c36ce81f9da
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-02-28 15:00:35 +0000

    update script n2

--- proj1/src/compile_and_test.bat
+++ proj1/src/compile_and_test.bat
@@ -2,6 +2,9 @@ mkdir out
 
 javac files/*.java messages/*.java peer/*.java tasks/*.java -d .\out\
 
+start cmd /k java -jar McastSnooper.jar 224.1.1.1:81 224.1.1.2:82 224.1.1.3:83
+timeout /t 1
+
 cd out
 
 for /l %%x in (1, 1, 5) do (
