6d5ae08b9f47feb9b44b885ffadb014f46fa6dd3
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-02-28 14:58:25 +0000

    update testing script

--- proj1/src/compile_and_test.bat
+++ proj1/src/compile_and_test.bat
@@ -5,7 +5,7 @@ javac files/*.java messages/*.java peer/*.java tasks/*.java -d .\out\
 cd out
 
 for /l %%x in (1, 1, 5) do (
-   timeout /t 1
+   rem timeout /t 1
    start cmd /k java peer.Peer 1.0 peer%%x sap 224.1.1.1:81 224.1.1.2:82 224.1.1.3:83
 )
 
