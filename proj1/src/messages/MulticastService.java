a061f3ab00f87561f9b68d741c93fe9d63a505b0
Gonçalo Teixeira <up201806562@fe.up.pt>
2021-03-27 21:33:47 +0000

    messages documentation done

--- proj1/src/messages/MulticastService.java
+++ proj1/src/messages/MulticastService.java
@@ -9,11 +9,23 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.util.Arrays;
 
+/**
+ * Class responsible to Send and Receive messages through multicast
+ */
 public class MulticastService extends MulticastSocket implements Runnable {
     private final InetAddress address;
     private final Peer peer;
     private final String identifier;
 
+    /**
+     * Constructor for the multicast service
+     *
+     * @param address    Address for the multicast socket
+     * @param port       Port for the multicast socket
+     * @param peer       Peer controlling the socket
+     * @param identifier Service ID (MC | MDB | MDR)
+     * @throws IOException On error creating the socket
+     */
     public MulticastService(InetAddress address, int port, Peer peer, String identifier) throws IOException {
         super(port);
         this.address = address;
@@ -22,8 +34,15 @@ public class MulticastService extends MulticastSocket implements Runnable {
 
         this.setTimeToLive(1);
         this.joinGroup(this.address);
+        System.out.printf("[MULTICAST SERVICE] [%s] Service is now Online\n", this.identifier);
     }
 
+    /**
+     * Method to send a message through multicast
+     *
+     * @param message Message to be sent
+     * @return true if the sending operation is successful
+     */
     public boolean sendMessage(Message message) {
         byte[] buffer = message.encodeToSend();
 
@@ -38,6 +57,9 @@ public class MulticastService extends MulticastSocket implements Runnable {
         }
     }
 
+    /**
+     * Method to be receiving packets and sending them to the dispatcher for triage right as they arrive
+     */
     @Override
     public void run() {
         while (true) {
--- proj1/src/messages/StoredMessage.java
+++ proj1/src/messages/StoredMessage.java
@@ -7,11 +7,16 @@ import tasks.Task;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 
+/**
+ * STORED Message Data Class
+ */
 public class StoredMessage extends Message {
+    //! Not documented
     public StoredMessage(String protocolVersion, int senderId, String fileId, int chunkNo) {
         super(protocolVersion, "STORED", senderId, fileId, chunkNo, 0, new byte[0]);
     }
 
+    //! Not documented
     @Override
     public byte[] encodeToSend() {
         return String.format("%s %s %s %s %d \r\n\r\n",
@@ -22,11 +27,13 @@ public class StoredMessage extends Message {
                 this.chunkNo).getBytes(StandardCharsets.UTF_8);
     }
 
+    //! Not documented
     @Override
     public Task createTask(Peer peer) {
         return new StoredTask(this, peer);
     }
 
+    //! Not documented
     @Override
     public ExecutorService getWorker(Peer peer) {
         return peer.getAcknowledgmentsExecutor();
